2b47ce1e61e60de9b7fb27b6dda10d65848eecf7
Khoerurrizal <kuroi.nichirin@gmail.com>
2023-01-26 11:58:00 +0700

    tambahkan authorization with api_key

--- product-management/app/Http/Controllers/Controller.php
+++ product-management/app/Http/Controllers/Controller.php
@@ -8,12 +8,16 @@ use Illuminate\Http\Request;
 class Controller extends BaseController
 {
     public function updateProduct(Request $request, $productId) {
-        
+        $user = $request->user();
+        if ($user == null){
+            return response()->json(['error' => 'Unauthorized'], 401, ['X-Header-One' => 'Header Value']);
+        }
         return response()->json([
             "id"=> $productId,
             "msg"=> "Berhasil update data",
             "data"=> $request->all(),
-            "name"=> $request->input('name')
+            "name"=> $request->input('name'),
+            "user"=> $user
         ]);
     }
 }
